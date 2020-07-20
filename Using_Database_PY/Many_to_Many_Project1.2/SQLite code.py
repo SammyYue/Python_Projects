93d9f649caef16fdfa98595d552cd9a0154447f6
Samantha T <60528803+SammyYue@users.noreply.github.com>
2020-07-19 22:31:05 -0600

    Add files via upload

---
+++ Using_Database_PY/Many_to_Many_Project1.2/SQLite code.py
@@ -0,0 +1,5 @@
+#SQL command code for a data
+SELECT hex(User.name || Course.title || Member.role ) AS X FROM
+    User JOIN Member JOIN Course
+    ON User.id = Member.user_id AND Member.course_id = Course.id
+    ORDER BY X
